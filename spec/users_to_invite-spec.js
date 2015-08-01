712dac1c40e63dd7e68baa2d3ee5596463f2cb7b
lisssb <liss.sb@gmail.com>
2015-08-01 20:53:46 +0200

    - convertToRadians function added

--- spec/users_to_invite-spec.js
+++ spec/users_to_invite-spec.js
@@ -1,1 +1,23 @@
 var user = require("../users_to_invite.js");
+describe('Users to invate test', function(){
+  describe('toRadians', function(){
+    
+    describe("convert positive grades to radian", function () {
+      it("should convert 30 grades to radian", function () {
+        var radians = user.toRadians(30);
+        expect(radians).toBe(0.5235987755982988);
+      });
+      it("should convert 125 grades to radian", function () {
+        var radians = user.toRadians(125);
+        expect(radians).toBe(2.1816615649929116);
+      });
+    });
+
+    describe("convert negatives grades to radian", function () {
+      it("should convert -15 grades to radian", function () {
+        var radians = user.toRadians(-15);
+        expect(radians).toBe(-0.2617993877991494);
+      });
+    });
+  });
+});
--- users_to_invite.js
+++ users_to_invite.js
@@ -1,4 +1,15 @@
 (function(){
-  var result;
+
+  /**
+  @param{number} angle
+  @return{number} The angle given converted to radian.
+  **/
+  var toRadians = function(angle){
+    return angle * Math.PI / 180;
+  };
+
+  var result = {
+    toRadians : toRadians
+  };
   module.exports = result;
 })()
