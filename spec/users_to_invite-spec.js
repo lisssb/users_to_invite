96311e0ca44268251aded88438c0a5f6d0863315
lisssb <liss.sb@gmail.com>
2015-08-01 21:22:38 +0200

    getDistance function added

--- spec/users_to_invite-spec.js
+++ spec/users_to_invite-spec.js
@@ -1,7 +1,6 @@
 var user = require("../users_to_invite.js");
 describe('Users to invate test', function(){
   describe('toRadians', function(){
-    
     describe("convert positive grades to radian", function () {
       it("should convert 30 grades to radian", function () {
         var radians = user.toRadians(30);
@@ -20,4 +19,24 @@ describe('Users to invate test', function(){
       });
     });
   });
+
+
+  describe('getDistance', function(){
+    it('The distance should be 43559.47257088365', function(){
+      var distance = user.getDistance(52.966, -6.463, 53.3381985, -6.2592576);
+      expect(distance).toBe(43559.47257088365);
+    });
+    it('The distance should be 41676.83909574573', function(){
+      var distance = user.getDistance(52.986375, -6.043701, 53.3381985, -6.2592576);
+      expect(distance).toBe(41676.83909574573);
+    });
+    it('The distance should be 82634.79317536046', function(){
+      var distance = user.getDistance(53.761389, -7.2875, 53.3381985, -6.2592576);
+      expect(distance).toBe(82634.79317536046);
+    });
+  });
+
+  
+
+
 });
--- users_to_invite.js
+++ users_to_invite.js
@@ -8,8 +8,43 @@
     return angle * Math.PI / 180;
   };
 
+  /**
+  @param{number} lat1
+  @param{number} lon1
+  @param{number} lat2
+  @param{number} lon2
+  @return{number} distance_between two points
+  **/
+  var getDistance = function(lat1, lon1, lat2, lon2){
+    var R = 6371000; // earth radius
+    var lat1 = toRadians(lat1);
+    var lon1 = toRadians(lon1);
+    var lat2 = lat2 || 53.3381985;
+    var lon2 = lon2 || -6.2592576;
+    var lat_abs_difference;
+    var lon_abs_difference;
+
+
+    lat2 = toRadians(lat2);
+    lon2 = toRadians(lon2);
+    lat_abs_difference = Math.abs(lat1 - lat2);
+    lon_abs_difference = Math.abs(lon1 - lon2);
+
+    var central_angle_aux = Math.sin(lat_abs_difference/2) * Math.sin(lat_abs_difference/2) +
+                        Math.cos(lat1) * Math.cos(lat2) *
+                        Math.sin(lon_abs_difference/2) * Math.sin(lon_abs_difference/2);
+    var central_angle = 2 * Math.asin
+    (Math.sqrt(central_angle_aux));
+    return R * central_angle;
+  };
+
+
+
+
+
   var result = {
-    toRadians : toRadians
+    toRadians : toRadians,
+    getDistance : getDistance
   };
   module.exports = result;
 })()
